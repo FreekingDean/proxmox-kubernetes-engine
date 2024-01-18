6dfbc6820d96d6ddc445de3f4f8bab577cf87cd4
Dean Galvin <deangalvin3@gmail.com>
2024-01-18 17:42:17 -0500

    Adds initial stuffs

---
+++ api-linter.sh
@@ -0,0 +1,14 @@
+set -eu
+
+temp_dir="$(mktemp -d)"
+descriptors_path=$temp_dir/descriptors.textpb
+buf build -o "${descriptors_path}" --as-file-descriptor-set
+
+cd protos
+all=$(find -name *.proto)
+api-linter \
+    --descriptor-set-in "${descriptors_path}" \
+    --config ../api-linter.yml \
+    ${all}
+
+rm -r "${temp_dir}"
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FreekingDean/proxmox-kubernetes-engine
+
+go 1.21.4
